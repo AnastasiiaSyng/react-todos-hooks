cc5b5d86d2d190aea751cbaa5c7b29a8eb7a4481
Anastasiia Synhaievska <anastasiia_synhaievska@epam.com>
2020-06-23 14:04:16 +0300

    Updates

---
+++ precache-manifest.4e62c88496459eec573e8599957dbbed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4733ffc3424a9a7df869415b6f113ebe",
+    "url": "/react-todos-hooks/index.html"
+  },
+  {
+    "revision": "946923511ebee7ac5fd1",
+    "url": "/react-todos-hooks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cfa3af2d2ebefde01d6f",
+    "url": "/react-todos-hooks/static/css/main.6572394f.chunk.css"
+  },
+  {
+    "revision": "946923511ebee7ac5fd1",
+    "url": "/react-todos-hooks/static/js/2.b07e1ceb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-todos-hooks/static/js/2.b07e1ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa3af2d2ebefde01d6f",
+    "url": "/react-todos-hooks/static/js/main.f8e5e1d9.chunk.js"
+  },
+  {
+    "revision": "5c2bf22c8d1b4a603443",
+    "url": "/react-todos-hooks/static/js/runtime-main.15300f73.js"
+  }
+]);
